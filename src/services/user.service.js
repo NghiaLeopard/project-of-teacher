20de8343c5be2248249059f256afed829609f6df
DaiNghiadz~ <nghiabeo1605gmail.com0>
2024-06-12 09:38:15 +0700

    change api

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,20 +1,24 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = "http://localhost:8080/api/test/";
-const API_URL_2 = "http://localhost:8080/api";
+const API_URL = "https://project-of-teacher-be.onrender.com/api/test/";
+const API_URL_2 = "https://project-of-teacher-be.onrender.com/api";
 // const getPublicContent = () => {
 //   return axios.get(API_URL + "all");
 // };
 const getAll = (params) => {
-  return axios.get(API_URL_2+"/user", { params },{ headers: authHeader() });
+    return axios.get(
+        API_URL_2 + "/user",
+        { params },
+        { headers: authHeader() }
+    );
 };
-const remove = id => {
-  return axios.delete(API_URL_2+`/user/${id}`,{ headers: authHeader() });
+const remove = (id) => {
+    return axios.delete(API_URL_2 + `/user/${id}`, { headers: authHeader() });
 };
 
-const get = id => {
-  return axios.get(API_URL_2+`/user/${id}`,{ headers: authHeader() });
+const get = (id) => {
+    return axios.get(API_URL_2 + `/user/${id}`, { headers: authHeader() });
 };
 // const getUserBoard = () => {
 //   return axios.get(API_URL + "employee", { headers: authHeader() });
@@ -29,13 +33,13 @@ const get = id => {
 // };
 
 const UserService = {
-  // getPublicContent,
-  // getUserBoard,
-  // getModeratorBoard,
-  // getAdminBoard,
-  get,
-  getAll,
-  remove,
+    // getPublicContent,
+    // getUserBoard,
+    // getModeratorBoard,
+    // getAdminBoard,
+    get,
+    getAll,
+    remove,
 };
 
 export default UserService;
